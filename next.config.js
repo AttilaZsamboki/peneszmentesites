cd28158eb01a519a1a29081170031698593fb624
AttilaZsamboki <zsamboki.attila.jr@gmail.com>
2023-08-20 09:07:16 +0200

    generate images directory

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
 // next.config.js
-const nextConfig = {
-	async headers() {
-		return [
-			{
-				source: "/(.*)", // Apply the header to all routes
-				headers: [
-					{
-						key: "Content-Security-Policy",
-						value: "upgrade-insecure-requests",
-					},
-				],
-			},
-		];
-	},
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
