1e931a32381c1e642019bbd8c869db4ee84f9fba
AttilaZsamboki <zsamboki.attila.jr@gmail.com>
2023-08-20 07:13:43 +0200

    enable http requests

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
 const nextConfig = {
+	async headers() {
+		return [
+			{
+				source: "/(.*)", // Apply the header to all routes
+				headers: [
+					{
+						key: "Content-Security-Policy",
+						value: "upgrade-insecure-requests",
+					},
+				],
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
